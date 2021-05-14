85885e045f50b5b5c0d663a1608a0914032aa5ab
Dheny Asnan <77433053+DhenyAsnan@users.noreply.github.com>
2021-05-14 16:18:21 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+/* Social Share Links
+Whatsapp
+https://api.whatsapp.com/send?text=[post-title] [post-url]
+
+Facabook
+https://www.facebook.com/sharer.php?u=[post-url]
+
+Twitter
+https://twitter.com/share?url=[post-url]&text=[post-title]&via=[via]&hashtags=[hashtags]
+
+Pinterest
+https://pinterest.com/pin/create/bookmarklet/?media=[post-img]&url=[post-url]&is_video=[is_video]&description=[post-title]
+
+
+
+*/
+
+const facebookBtn = document.querySelector(".facebook-btn");
+const twitterBtn = document.querySelector(".twitter-btn");
+const whatsappBtn = document.querySelector(".whatsapp-btn");
+const pinterestBtn = document.querySelector(".pinterest-btn");
+
+function init() {
+  //   console.log("hello");
+
+  const pinterestImg = document.querySelector(".pinterest-img");
+
+  let postUrl = encodeURI(document.location.href);
+  let postTitle = encodeURI("Hi everyone, plase check this out: ");
+  let postImg = encodeURI(pinterestImg.src);
+
+  facebookBtn.setAttribute("href", "https://www.facebook.com/sharer.php?u=${postUrl}");
+  twitterBtn.setAttribute("href", "https://twitter.com/intent/tweet?url=${postUrl}&text=${postTitle}");
+  whatsappBtn.setAttribute("href", "https://api.whatsapp.com/send?text=${postTitle} ${postUrl}");
+  pinterestBtn.setAttribute("href", "https://pinterest.com/pin/create/bookmarklet/?media=${postImg}&url=${postUrl}&description=${postTitle}");
+}
+init();
