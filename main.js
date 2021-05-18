65051a0a77a9a1f41b341d80718e9183b4039046
Dheny Asnan <77433053+DhenyAsnan@users.noreply.github.com>
2021-05-19 02:43:24 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ function init() {
   let postTitle = encodeURI("Hi everyone, plase check this out:");
 
   facebookBtn.setAttribute("href", "https://www.facebook.com/sharer.php?u=");
-  twitterBtn.setAttribute("href", "https://twitter.com/share?url=${postUrl}&text=${postTitle}");
-  whatsappBtn.setAttribute("href", "https://api.whatsapp.com/send?text=${postTitle} ${postUrl}");
+  twitterBtn.setAttribute("href", "https://twitter.com/share?url=");
+  whatsappBtn.setAttribute("href", "https://api.whatsapp.com/send?text=");
 }
 init();
