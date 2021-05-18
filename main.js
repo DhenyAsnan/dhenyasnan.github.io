7a9e37bfeda1c7dd25563cfacfe9a178eda6987e
Dheny Asnan <77433053+DhenyAsnan@users.noreply.github.com>
2021-05-19 02:37:48 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -18,20 +18,15 @@ https://pinterest.com/pin/create/bookmarklet/?media=[post-img]&url=[post-url]&is
 const facebookBtn = document.querySelector(".facebook-btn");
 const twitterBtn = document.querySelector(".twitter-btn");
 const whatsappBtn = document.querySelector(".whatsapp-btn");
-const pinterestBtn = document.querySelector(".pinterest-btn");
 
 function init() {
-  //   console.log("hello");
-
-  const pinterestImg = document.querySelector(".pinterest-img");
+  // console.log("hello");
 
   let postUrl = encodeURI(document.location.href);
-  let postTitle = encodeURI("Hi everyone, plase check this out: ");
-  let postImg = encodeURI(pinterestImg.src);
+  let postTitle = encodeURI("Hi everyone, plase check this out:");
 
-  facebookBtn.setAttribute("href", "https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdhenyasnan.github.io/gajah.html");
-  twitterBtn.setAttribute("href", "https://twitter.com/intent/tweet?via=dhenyasnan&text=Tetap%20Optimis%20di%20Tengah%20Pandemi%20COVID%20-19%20&url=https%3A%2F%2Fdhenyasnan.github.io%2Fgajah.html%2F");
-  whatsappBtn.setAttribute("href", "https://api.whatsapp.com/send?text=Tetap%20Optimis%20di%20Tengah%20Pandemi%20COVID%20-19%20https%3A%2F%2Fdhenyasnan.github.io/gajah.html");
-  pinterestBtn.setAttribute("href", "#");
+  facebookBtn.setAttribute("href", "https://www.facebook.com/sharer.php?u=");
+  twitterBtn.setAttribute("href", "https://twitter.com/share?url=${postUrl}&text=${postTitle}");
+  whatsappBtn.setAttribute("href", "https://api.whatsapp.com/send?text=${postTitle} ${postUrl}");
 }
 init();
